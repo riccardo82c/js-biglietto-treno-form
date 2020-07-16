3aceb3f90d6e5ef0a315f3ffed5d43588dcb0f25
Riccardo Colombo <rikyregna@gmail.com>
2020-07-16 16:18:57 +0200

    lista della spesa JS

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// rifare esercizio biglietto treno
+// 1. devo acquisire le variabili da input
+// 2. creare una funzione che calcola il prezzo del biglietto (prezzo * km * sconto età)
+// 3. generare una funzione che mi calcola il numero random in un intervallo
+// 4. al click del pulsante genera stampare i risultati nella sezione bottom
+// 4.1 aggiungere classe show alla sezione bottom (inizialmente hidden)
+// 4.2 aggiungere animazione al click
+// 5. al click di annulla ripulire il form
+// 5.1 aggiungere classe hidden alla sezione bottom (per farla tornare hidden)
